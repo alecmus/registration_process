dff245e5ea6274b6e870486bb50831685be8d700
Alec Musasa <alecmus@live.com>
2024-01-25 17:22:52 +0200

    Added deployment code

--- src/main/java/com/github/alecmus/registration_process/RegistrationProcessApplication.java
+++ src/main/java/com/github/alecmus/registration_process/RegistrationProcessApplication.java
@@ -1,9 +1,11 @@
 package com.github.alecmus.registration_process;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Deployment(resources = {"classpath*:*.form", "classpath*:*.bpmn"})
 public class RegistrationProcessApplication {
 
     public static void main(String[] args) {
